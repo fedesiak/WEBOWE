2a372e75b18444d63c57ec6b132523f3dcf034f7
fedesiak <120318227+fedesiak@users.noreply.github.com>
2025-10-28 08:32:08 +0100

    Add files via upload

---
+++ cw4_mysql/Controllers/BooksStoreController.cs
@@ -0,0 +1,37 @@
+using cw4_mysql.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw4_mysql.Controllers
+{
+    public class BooksStoreController : Controller
+    {
+        private BookRepo _db;
+        public BooksStoreController(IConfiguration config)
+        {
+            _db = new BookRepo(config);
+        }
+
+        // GET: BooksStoreController
+        public ActionResult Index()
+        {
+            var books = _db.GetBooks();
+            return View(books);
+        }
+
+        public IActionResult AddBook()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddBook(Book book)
+        {
+            if(ModelState.IsValid)
+            {
+                //zapisanie do bazy
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
