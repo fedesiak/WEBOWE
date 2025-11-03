3f0eb49c0b345ee0a834880915cecddf772baa32
fedesiak <120318227+fedesiak@users.noreply.github.com>
2025-11-03 15:01:03 +0100

    Add files via upload

---
+++ zadanie1 3.11.2025/cw4_mysql/Controllers/BooksStoreController.cs
@@ -0,0 +1,72 @@
+using cw4_mysql.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw4_mysql.Controllers
+{
+    public class BooksStoreController : Controller
+    {
+        private BookRepo _db;
+        public BooksStoreController(IConfiguration config)
+        {
+            _db = new BookRepo(config);
+        }
+
+        // GET: BooksStoreController
+        public ActionResult Index()
+        {
+            var books = _db.GetBooks();
+            return View(books);
+        }
+        [HttpGet]
+        public IActionResult AddBook()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddBook(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                //zapisanie do bazy
+                _db.AddBook(book);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            //usuwanie z bazy
+            _db.DeleteBook(id);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            //pobranie z bazy do edycji
+            var bookToEdit = _db.GetBookById(id);
+            if (bookToEdit == null)
+            {
+                //powrot do listy gdy nie znaleziono ksiazki do edycji
+                return RedirectToAction("Index");
+            }
+            //wyslanie do widoku edycji z formularzem
+            return View(bookToEdit);
+        }
+        [HttpPost]
+
+
+        public IActionResult Edit(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                 //zapisanie do bazy po edycji
+                 _db.UpdateBook(book);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }   
+
+
+
+    }
+}
